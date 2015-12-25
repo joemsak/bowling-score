819e4e69b18396fc2aff96ef5148593f48978c06
Joe Sak <joe@joesak.com>
2015-12-25 11:21:19 -0500

    Keeps the score

--- lib/bowling/score.rb
+++ lib/bowling/score.rb
@@ -1,7 +1,88 @@
 require "bowling/score/version"
 
 module Bowling
-  module Score
-    # Your code goes here...
+  class ScoreKeeper
+    def initialize(*players)
+      @players = players
+      @frames = {}
+      @current_frame = 0
+
+      @players.collect do |player|
+        @frames[player] = []
+      end
+    end
+
+    def roll(pins, player)
+      frame = @frames[player][@current_frame] || []
+      frame << frame_chances(frame, pins)
+      @frames[player][@current_frame] = frame
+      @current_frame += 1 if !tenth_frame? && (frame.size == 2 || pins == 10)
+    end
+
+    def score(player)
+      score = 0
+      frames = @frames[player]
+
+      frames.each_with_index do |frame, i|
+        score += score_frame(frames, frame, i)
+      end
+
+      score
+    end
+
+    private
+    def frame_chances(frame_chances, pins)
+      if frame_chances.nil?
+        pins
+      elsif tenth_frame?
+        score_tenth_frame(frame_chances, pins)
+      else
+        pins
+      end
+    end
+
+    def score_tenth_frame(frame, pins)
+      if (frame.size == 2 && frame.inject(:+) < 10) || frame.size == 3
+        raise PlayerGameHasEnded
+      else
+        pins
+      end
+    end
+
+    def score_frame(frames, frame, i)
+      if i == 9 # tenth frame
+        frame.inject(:+)
+      elsif strike?(frame)
+        strike_bonus(frames, i)
+      elsif spare?(frame)
+        spare_bonus(frames, i)
+      else
+        frame.inject(:+)
+      end
+    end
+
+    def strike?(frame)
+      frame == [10]
+    end
+
+    def strike_bonus(frames, i)
+      next_frame = frames[i + 1]
+      second_ball = next_frame[1] ? next_frame[1] : frames[i + 2][0]
+      10 + next_frame[0] + second_ball
+    end
+
+    def spare?(frame)
+      frame.size == 2 && frame.inject(:+) == 10
+    end
+
+    def spare_bonus(frames, i)
+      10 + frames[i + 1][0]
+    end
+
+    def tenth_frame?
+      @current_frame == 9
+    end
   end
+
+  class PlayerGameHasEnded < StandardError; end
 end
--- spec/bowling/score_spec.rb
+++ spec/bowling/score_spec.rb
@@ -1,11 +1,86 @@
 require 'spec_helper'
 
-describe Bowling::Score do
-  it 'has a version number' do
-    expect(Bowling::Score::VERSION).not_to be nil
+describe Bowling::ScoreKeeper do
+  let(:score_keeper) { Bowling::ScoreKeeper.new('Rick', 'Morty') }
+
+  it 'rolls for a player' do
+    score_keeper.roll(0, 'Rick')
+    expect(score_keeper.score('Rick')).to eq(0)
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  it 'rolls for 2 players' do
+    score_keeper.roll(0, 'Rick')
+    score_keeper.roll(9, 'Morty')
+
+    expect(score_keeper.score('Morty')).to eq(9)
+  end
+
+  it 'rolls for a frame' do
+    score_keeper.roll(1, 'Rick')
+    score_keeper.roll(8, 'Rick')
+
+    expect(score_keeper.score('Rick')).to eq(9)
+  end
+
+  it 'scores a game of gutters' do
+    20.times { score_keeper.roll(0, 'Rick') }
+    expect(score_keeper.score('Rick')).to eq(0)
+  end
+
+  it 'scores a game of 1s' do
+    20.times { score_keeper.roll(1, 'Rick') }
+    expect(score_keeper.score('Rick')).to eq(20)
+  end
+
+  it 'scores a game of 1s and a spare' do
+    17.times { score_keeper.roll(1, 'Rick') } # terrible 8.5 frames
+    score_keeper.roll(9, 'Rick')              # spare in the 9th frame
+    2.times { score_keeper.roll(1, 'Rick') }  # terrible tenth frame
+
+    expect(score_keeper.score('Rick')).to eq(29)
+  end
+
+  it 'scores a game of 1s and a strike' do
+    16.times { score_keeper.roll(1, 'Rick') } # terrible 8 frames
+    score_keeper.roll(10, 'Rick')             # strike in the 9th frame
+    2.times { score_keeper.roll(1, 'Rick') }  # terrible tenth frame
+
+    expect(score_keeper.score('Rick')).to eq(30)
+  end
+
+  it 'scores multiple strikes' do
+    14.times { score_keeper.roll(1, 'Rick') } # terrible 7 frames
+    score_keeper.roll(10, 'Rick')             # strike in the 8th frame
+    score_keeper.roll(10, 'Rick')             # strike in the 9th frame
+    2.times { score_keeper.roll(1, 'Rick') }  # terrible tenth frame
+
+    expect(score_keeper.score('Rick')).to eq(49)
+  end
+
+  it 'scores a perfect game' do
+    12.times { score_keeper.roll(10, 'Rick') }
+    expect(score_keeper.score('Rick')).to eq(300)
+  end
+
+  it 'plays a boring 10th frame' do
+    9.times { score_keeper.roll(10, 'Rick') }
+    2.times { score_keeper.roll(1, 'Rick') }
+
+    expect(score_keeper.score('Rick')).to eq(245)
+    expect {
+      score_keeper.roll(1, 'Rick')
+    }.to raise_error(Bowling::PlayerGameHasEnded)
+  end
+
+  it 'plays a spared 10th frame' do
+    9.times { score_keeper.roll(10, 'Rick') }
+    score_keeper.roll(1, 'Rick')
+    score_keeper.roll(9, 'Rick')
+    score_keeper.roll(5, 'Rick')
+
+    expect(score_keeper.score('Rick')).to eq(266)
+    expect {
+      score_keeper.roll(1, 'Rick')
+    }.to raise_error(Bowling::PlayerGameHasEnded)
   end
 end
